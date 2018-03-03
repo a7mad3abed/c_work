0d56e0e7b826a954daaf12614568832762a35d6e
a7mad3abed <a7mad3abed@gmail.com>
2018-03-03 21:20:35 +0100

    changed main

--- mylib/src/main.c
+++ mylib/src/main.c
@@ -1,10 +1,48 @@
 #include "../include/mylib.h"
 
+void disp_all();
+void remove_book();
+void new_book();
 
 int main()
 {
 	display();
-	loop();
+
+		while ((c = getchar()) != 'q')
+		{
+			display();
+			switch (c)
+			{
+				case 'd':
+					disp_all();
+					break;
+				case 'n':
+					new_book();
+					break;
+				case 'r':
+					remove_book();
+					break;
+				case 'q':
+					break;
+				default:
+					mvprintw(LINES, COLS, "not a valid option");
+			}
+		}
+
 }
 
+void disp_all()
+{
+	mvprintw(LINES-2, 0, "all displayed");
 
+}
+
+void new_book()
+{
+	mvprintw(LINES-2, 0, "new book created                ");
+}
+
+void remove_book()
+{
+	mvprintw(LINES-2, 0, "selected book removed successfully");
+}

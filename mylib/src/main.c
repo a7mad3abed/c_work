b889758add1be73b7566c6d1650e99cf5d385f52
a7mad3abed <a7mad3abed@gmail.com>
2018-03-03 22:21:53 +0100

    mod main.c

--- mylib/src/main.c
+++ mylib/src/main.c
@@ -3,6 +3,7 @@
 void disp_all();
 void remove_book();
 void new_book();
+void  display();
 
 int main()
 {
@@ -45,4 +46,23 @@ void new_book()
 void remove_book()
 {
 	mvprintw(LINES-2, 0, "selected book removed successfully");
+}
+
+void display()
+{
+	initscr();
+	noecho();
+	cbreak();
+	char *welcome = "Welcome to my lib, a program to help sorting your books";
+	int y, x;
+	y = LINES/2;
+	x = (COLS-strlen(welcome))/2;
+
+
+	mvprintw(y, x, welcome);
+	mvprintw(++y, x+1, "to display all books please press d");
+	mvprintw(++y, x+1, "to add a new book please press n");
+	mvprintw(++y, x+1, "to remove an existing book please press r");
+	mvprintw(++y, x+1, "to quit the program please press q");
+	endwin();
 }

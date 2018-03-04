da8c49364ad6e7a46bae022e5d9ffdb191e8fae8
U-DESKTOP-1QDV9RI\a7mad <a7mad3abed@gmail.com>
2018-03-04 03:01:29 +0100

    ldfjk

--- mylib/include/mylib.h
+++ mylib/include/mylib.h
@@ -1,16 +1,13 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
 #include <ncurses.h>
 #include <string.h>
 #include <stdlib.h>
 
-#ifndef MYLIB_H
-#define MYLIB_H
 
-void display();
-void loop();
-void new_book();
-void remove_book();
-void disp_all();
-int c;
+
+
 
 
 #endif
